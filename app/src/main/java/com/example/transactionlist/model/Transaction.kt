a76a8cec7ed1daa80d66a7ea05ff62370125fa40
Irya <iryadev15@gmail.com>
2023-11-19 01:37:09 +0700

    Create Transaction model

---
+++ app/src/main/java/com/example/transactionlist/model/Transaction.kt
@@ -0,0 +1,9 @@
+package com.example.transactionlist.model
+
+data class Transaction(
+    val title: String,
+    val status: String,
+    val updatedAt: String,
+    val item: Int,
+    val total: String,
+)
